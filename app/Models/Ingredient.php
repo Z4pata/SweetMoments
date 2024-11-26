dca93f283a30f3a3e01ff994e1012f65c9b73b88
Zapata <zapata.devs@gmail.com>
2024-11-26 18:54:29 -0500

    Feat: Ingredients table and seeders

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    /** @use HasFactory<\Database\Factories\IngredientFactory> */
+    use HasFactory;
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | GLOBAL VARIABLES
+    |--------------------------------------------------------------------------
+    */
+
+    // protected $table = 'categories';
+    // protected $primaryKey = 'id';
+    public $timestamps = false;
+    // protected $guarded = [];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+        ];
+    }
+
+
+    
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    // protected $hidden = [
+    //     
+    // ];
+
+
+
+    // protected $dates = [];
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTIONS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | SCOPES
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESORS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | MUTATORS
+    |--------------------------------------------------------------------------
+    */
+}
---
+++ database/seeders/IngredientSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ingredient;
+use Illuminate\Database\Seeder;
+
+class IngredientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $ingredients = [
+            ['name' => 'flour'],
+            ['name' => 'chocolate'],
+            ['name' => 'vanilla'],
+            ['name' => 'strawberries'],
+            ['name' => 'sugar'],
+            ['name' => 'peach'],
+            ['name' => 'raisins'],
+        ];
+
+        foreach ($ingredients as $ingredient)
+        {
+            Ingredient::create($ingredient);
+        }
+    }
+}
